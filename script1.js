38a5de66bcd3de92e8133a1695e40c6e38f96468
Huma Zia <rankwithhani@gmail.com>
2024-12-18 12:49:37 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,21 @@
+//Practice Test Chappter 3
+
+//Answer of Question 1
+var age = "I am 27 Years old"
+alert(age);
+
+//Answer of Question 2 
+ let visitCount = 14;
+ alert("You have visited this site " + visitCount + " times");
+
+ //Answer of Question 3
+ var birthYear = "1997"
+ document.write("My birth year is " + birthYear);
+
+ //Answer of Question 4
+ var visitorName = "Huma Zia";
+ var quantity = "Ordered 5"
+ var productTitle = "Tote Bags";
+ var companyName = " from Hani's Store";
+
+ document.write(visitorName + " " + quantity + " " + productTitle + " " + companyName);
