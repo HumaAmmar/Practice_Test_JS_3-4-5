178ebb94ec93f113f888c121911135520083963b
Huma Zia <rankwithhani@gmail.com>
2024-12-18 13:37:41 +0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,74 @@
+//Practice test of Chapter 5
+
+
+//Answer of Question 1
+var num = 4;
+var num1 = 9;
+var result = num + num1;
+
+// document.write(result); 
+
+//Answer of Question 2
+var num = 4;
+var num1 = 9;
+var result = num - num1;
+
+
+var num = 4;
+var num1 = 9;
+var result = num * num1;
+
+var num = 4;
+var num1 = 9;
+var result = num / num1;
+
+var num = 4;
+var num1 = 9;
+var result = num % num1;
+
+//Answer off Question 3
+//a
+var number;
+
+//b 
+document.write("Value after variable declaration is:" + number +"<br>");
+
+//c
+var number = 5;
+
+//d
+document.write("Initial value:" + number +"<br>");
+
+//e 
+var number = ++number
+
+//f
+document.write("Value after increment is:" + number +"<br>");
+
+//g
+var number1 = 7
+var result = number + number1;
+
+//h
+document.write("Value after addition is:" + result +"<br>");
+
+//i
+var result = --result;
+
+//j
+ document.write("Value after decrement is:" + result +"<br>");
+
+//k
+var result = 12 % 3;
+
+//l
+ document.write("Value after modulus is:" + result +"<br> <br>");
+
+
+
+
+//Answer of Question 4
+ var costOfMovieTickect = 600;
+ var costOfTotalTickets = costOfMovieTickect * 5;
+ 
+ document.write("Total Cost to buy 5 tickets is:" + costOfTotalTickets)
