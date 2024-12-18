1a53870160a4e4cdcb03c3f4d2b3915fdc18d5c1
Huma Zia <rankwithhani@gmail.com>
2024-12-18 12:46:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//Practice Test Chapter 4
+
+//Answer of Question 1 
+var fullName = "Huma Zia" , age = 27 , Gender = "Female" ;
+
+//Answer of Question 2
+// 5 legal Variables
+
+var myVar; 
+var personal_Alert;
+var $console;
+var passsword123;
+var userDocumentValidation;
+ 
+//5 Illegal Variables 
+
+// var var;
+// var 1name;
+// var Full name;
+// var true/false;
+// var Father's name;
+
+//Answer of Question 3
+document.write("Rules for naming JS variables <br>")
+
+document.write("1. Variable names can only contain letter, numbers, underscore (_), or dollar sign ($). <br>")
+
+document.write("2. Variable names should not start with a number. <br>")
+
+document.write("3. Variable names should not contain spaces or special characters other than underscore (_). <br>")
+
+document.write("4. Variable are case-sensitive (e.g., myVar and myvar are different variables). <br>")
+
+document.write("5. Variable names should not be  JS reserved keywords (e.g., var, let, const, function, etc.). <br>")
